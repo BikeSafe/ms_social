d20f80c702a899ef576d55c8e8d8f39bd305cb83
dacperezce <dacperezce@unal.edu.co>
2021-05-03 17:42:07 -0500

    arreglos

--- src/main/java/com/arquisoft1/controller/SocialRestController.java
+++ src/main/java/com/arquisoft1/controller/SocialRestController.java
@@ -1,16 +1,8 @@
 package com.arquisoft1.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,152 +23,35 @@ public class SocialRestController {
 	private ISocialService service;
 	
 	@GetMapping("/seguidores/{seguido}")
-	public ResponseEntity<?> findSeguidores(@PathVariable String seguido){
-		
-		List<String> list = null;
-		Map<String, Object> response = new HashMap<>();
-		
-		if(seguido.length() != 36) {
-			response.put("error", "uuid incorrecto");
-			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
-		}
-		
-		try {
-			list = service.findSeguidores(seguido);
-		}catch(DataAccessException e) {
-			response.put("mensaje", "Error al realizar la consulta");
-			response.put("error", e.getMessage());
-			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		
-		return new ResponseEntity<List<String>>(list, HttpStatus.OK);
+	public List<String> findSeguidores(@PathVariable String seguido){
+		return service.findSeguidores(seguido);
 	}
 	
 	@GetMapping("/seguidos/{seguidor}")
-	public ResponseEntity<?> findSeguidos(@PathVariable String seguidor){
-		
-		List<String> list = null;
-		Map<String, Object> response = new HashMap<>();
-		
-		if(seguidor.length() != 36) {
-			response.put("error", "uuid incorrecto");
-			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
-		}
-		
-		try {
-			list = service.findSeguidos(seguidor);
-		}catch(DataAccessException e) {
-			response.put("mensaje", "Error al realizar la consulta");
-			response.put("error", e.getMessage());
-			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		
-		return new ResponseEntity<List<String>>(list, HttpStatus.OK);
+	public List<String> findSeguidos(@PathVariable String seguidor){		
+		return service.findSeguidos(seguidor);
 	}
 	
 	@PostMapping("/crear")
-	public ResponseEntity<?> create(@Valid @RequestBody Social social, BindingResult result) {
-		
-		Social socialNew = null;
-		Map<String, Object> response = new HashMap<>();
-		
-		if(result.hasErrors()) {
-			
-			List<String> errors = result.getFieldErrors()
-					.stream()
-					.map(err -> "El campo " + err.getField() +" "+ err.getDefaultMessage())
-					.collect(Collectors.toList());
-			
-			response.put("error", errors);
-			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
-		}
-		
-		try {
-			socialNew =service.create(social);
-		}catch(DataAccessException e) {
-			response.put("mensaje", "Error al realizar la consulta");
-			response.put("error", e.getMessage());
-			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		
-		
-		return new ResponseEntity<Social>(socialNew, HttpStatus.CREATED);
+	public Social create(@RequestBody Social social) {		
+		return service.create(social);
 	}
 	
 	@GetMapping("/verificar")
-	public ResponseEntity<?> verify(@Valid @RequestBody Social social, BindingResult result) {
-		Social socialFind = null;
-		Map<String, Object> response = new HashMap<>();
-		
-		if(result.hasErrors()) {
-			
-			List<String> errors = result.getFieldErrors()
-					.stream()
-					.map(err -> "El campo " + err.getField() +" "+ err.getDefaultMessage())
-					.collect(Collectors.toList());
-			
-			response.put("error", errors);
-			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
-		}
-		
-		try {
-			socialFind = service.findSocial(social);
-		}catch(DataAccessException e) {
-			response.put("mensaje", "Error al realizar la consulta");
-			response.put("error", e.getMessage());
-			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		
+	public boolean verify(@RequestBody Social social, BindingResult result) {
+		Social socialFind = service.findSocial(social);	
 		
 		if(socialFind == null) {
-			return new ResponseEntity<Boolean>(false, HttpStatus.OK);
+			return false;
 		}else {
-			return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+			return true;
 		}
 	}
 	
 	@DeleteMapping("/eliminar")
-	public ResponseEntity<?> delete(@Valid @RequestBody Social social, BindingResult result) {
-		
-		Social socialFind = null;
-		Map<String, Object> response = new HashMap<>();
-		
-		if(result.hasErrors()) {
-			
-			List<String> errors = result.getFieldErrors()
-					.stream()
-					.map(err -> "El campo " + err.getField() +" "+ err.getDefaultMessage())
-					.collect(Collectors.toList());
-			
-			response.put("error", errors);
-			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
-		}
-		
-		try {
-			socialFind = service.findSocial(social);
-		}catch(DataAccessException e) {
-			response.put("mensaje", "Error al realizar la consulta");
-			response.put("error", e.getMessage());
-			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		
-		
-		if(socialFind == null) {
-			response.put("mensaje", "La relacion seguidor seguido no existe en la base de datos");
-			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
-		}else {
-			try {
-				service.delete(socialFind);
-				response.put("mensaje", "eliminacion exitosa");
-				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
-			}catch(DataAccessException e) {
-				response.put("mensaje", "Error al realizar la consulta");
-				response.put("error", e.getMessage());
-				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-			
-		}
-		
+	public void delete(@RequestBody Social social) {
+		Social socialFind = service.findSocial(social);	
+		service.delete(socialFind);
 	}
 	
 }
